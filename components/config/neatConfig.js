e6b54f4869314dd4bc587595adde764dbf96b988
jaycee808 <jamie.clark@live.co.uk>
2025-04-19 21:30:43 +0100

    adjusted gradient settings

--- components/config/neatConfig.js
+++ components/config/neatConfig.js
@@ -3,7 +3,7 @@ export const config = {
       // { color: '#4FC3F7', enabled: true }, // blue
       // { color: '#18FFFF', enabled: true }, // cyan
       { color: '#ECEFF1', enabled: true }, // blue-grey
-      { color: '#FAFAFA', enabled: true }, // grey
+      // { color: '#FAFAFA', enabled: true }, // grey
       { color: '#9FA8DA', enabled: true }, // indigo
       { color: '#3D5AFE', enabled: true }, // blue
       { color: '#E91E63', enabled: true }, // pink
@@ -13,19 +13,19 @@ export const config = {
     verticalPressure: 4,
     waveFrequencyX: 2,
     waveFrequencyY: 3,
-    waveAmplitude: 1,
+    waveAmplitude: 0.6,
     shadows: 1,
     highlights: 5,
     colorBrightness: 1,
-    colorSaturation: 7,
+    colorSaturation: 5,
     wireframe: false,
     colorBlending: 10,
     backgroundColor: '#003FFF',
     backgroundAlpha: 1,
     grainScale: 0,
     grainSparsity: 0.02,
-    grainIntensity: 0.125,
+    grainIntensity: 0.1,
     grainSpeed: 1,
-    resolution: 1,
+    resolution: 0.5,
     yOffset: 0,
   };  
