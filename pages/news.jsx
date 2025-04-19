658dc9519010c5ba227dc15e3f1b82f0f825070a
jaycee808 <jamie.clark@live.co.uk>
2025-04-19 14:05:00 +0100

    added news feed

--- pages/news.jsx
+++ pages/news.jsx
@@ -10,6 +10,10 @@ export default function News() {
                 <p className="font-body">
                 Roll up, roll up.
                 </p>
+                <div className="itflex justify-center">
+                    <script src="https://static.elfsight.com/platform/platform.js" async></script>
+                    <div className="elfsight-app-16d953f9-96ff-4d19-9d55-4897c8570fb0" data-elfsight-app-lazy></div>
+                </div>
             </main>
         <Footer />
         </>
